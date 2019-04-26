c7d977dc89ea2eeb8c4356ba8a3c2a64f53cc394
Jessica Roy <bouncingleaf@gmail.com>
2019-04-25 21:31:11 -0400

    Set up webpack, babel, eslint, react, and a simple dashboard

---
+++ src/app/index.jsx
@@ -0,0 +1,11 @@
+// import {store} from './store';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Main} from './components/Main';
+
+// Actually kicks off the application
+ReactDOM.render(
+    < Main/>,
+    document.getElementById('app')
+);
---
+++ src/server/defaultState.js
@@ -0,0 +1,65 @@
+export const defaultState = {
+    users:[{
+        id:"U1",
+        name:"Dev"
+    },
+    {
+        id:"U2",
+        name:"Bono"
+    }],
+    groups:[{
+        name:"To Do",
+        id:"G1",
+        owner:"U1"
+    },
+    {
+        name:"Other group",
+        id:"G2",
+        owner:"U2"
+    }],
+    tasks:[{
+        name:"Do tests",
+        id:"T1",
+        group:"G1",
+        owner:"U1",
+        isComplete:false
+    },
+    {
+        name:"Do other stuff",
+        id:"T2",
+        group:"G1",
+        owner:"U1",
+        isComplete:false
+    },
+    {
+        name:"Another task",
+        id:"T3",
+        group:"G2",
+        owner:"U2",
+        isComplete:false
+    }],
+    comments:[{
+        owner:"U1",
+        id:"C1",
+        task:"T1",
+        content:"Great!"
+    },
+    {
+        owner:"U1",
+        id:"C2",
+        task:"T2",
+        content:"Wow!"
+    },
+    {
+        owner:"U2",
+        id:"C3",
+        task:"T1",
+        content:"Hey!"
+    },
+    {
+        owner:"U2",
+        id:"C4",
+        task:"T2",
+        content:"Yay!"
+    }]
+}
