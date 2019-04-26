f2177e60d24202f3b573d7ffd3479587aba94c63
Jessica Roy <bouncingleaf@gmail.com>
2019-04-25 22:03:36 -0400

    Started the work of adding new tasks

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-export const TaskList = ({tasks, name}) => (
+import { requestTaskCreation } from '../store/mutations';
+
+export const TaskList = ({tasks, name, id, createNewTask}) => (
     <div>
       <h3>
         {name}
@@ -13,6 +15,7 @@ export const TaskList = ({tasks, name}) => (
               </div>
           ))}       
       </div>
+      <button onClick={() => createNewTask(id)}>Add New</button>
     </div>
 )
 
@@ -25,4 +28,13 @@ const mapStateToProps = (state, ownProps) => {
     }
 };
 
-export const ConnectedTaskList = connect(mapStateToProps) (TaskList);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+      createNewTask(id) {
+        console.log('Creating new task ' + id);
+        dispatch(requestTaskCreation(id));
+      }
+    }
+}
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps) (TaskList);
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
 import { defaultState } from '../../server/defaultState';
 
 export const store = createStore(
     function reducer(state = defaultState, action) {
     return state;
-    }
+    },
+    applyMiddleware(createLogger())
 );
