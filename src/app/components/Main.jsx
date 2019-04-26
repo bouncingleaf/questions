e336c74a2a7ac04148a6d090de6090b4cb7d6a05
Jessica Roy <bouncingleaf@gmail.com>
2019-04-25 21:46:43 -0400

    Add navigation

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Route, Router } from 'react-router-dom';
+
+import { history } from '../store/history';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard';
+import { ConnectedNavigation } from './Navigation';
 
 export const Main = () => (
+  <Router history={history}>
     <Provider store={store}>
       <div>
-        <ConnectedDashboard />
+        <ConnectedNavigation />
+        {/* <ConnectedDashboard /> */}
+        <Route
+          exact
+          path="/dashboard"
+          render={ ()=>(<ConnectedDashboard/>)}
+        />
       </div>
     </Provider>
+  </Router>
 )
