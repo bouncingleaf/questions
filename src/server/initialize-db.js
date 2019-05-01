179927adc0daa28f72c7dfc08c1b0b9986cf4171
Jessica Roy <bouncingleaf@gmail.com>
2019-05-01 19:59:43 -0400

    Updated initialization process

--- src/server/initialize-db.js
+++ src/server/initialize-db.js
@@ -10,17 +10,15 @@ async function initializeDB() {
   // Only initialize if it's empty, e.g., (default user not found)
   let user = await db.collection(`users`).findOne({id:"U1"});
   if (!user) {
+    console.log('Updating the database...');
 
     // Update the questions
-    console.log('Converting the CSV...');
     const jsonArray = await csv().fromFile(csvfile);
     let questions = db.collection("questions");
     // pass insertMany an array, and that will be inserted in the db if everything is valid
-    console.log('Inserting the questions into the database...');
     await questions.insertMany(jsonArray);
 
     // Update the users and any other data
-    console.log('Adding other application info to the database...');
     for (let collectionName in defaultState) {
       // collections are: users, groups, tasks, etc.
       let collection = db.collection(collectionName);
