5bdb8e7a137bc9a05c0174398637ad904d138011
Jessica Roy <bouncingleaf@gmail.com>
2019-05-01 22:48:57 -0400

    deployment

--- src/server/server.js
+++ src/server/server.js
@@ -11,10 +11,10 @@ let app = express();
 
 app.listen(port, console.log('Server listening on port', port));
 
-// A simple test:
-app.get('/', (req, res) => {
-  res.send("Hello world!!");
-})
+// // A simple test:
+// app.get('/', (req, res) => {
+//   res.send("Hello world!!");
+// })
 
 app.use(
   cors(),
